395329f770aff09714166b2ba69f5f3fd54dc111
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2021-11-05 03:04:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/dqanshi/EvaMaria
+  git clone https://github.com/dqanshi/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria.git /EvaMaria
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
