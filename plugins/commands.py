d4911b756d0a637ce95b1447697c8949f982620b
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2023-05-13 21:47:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -91,7 +91,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('⚜️Owner', url='https://t.me/am_dq_fa')
+            InlineKeyboardButton('⚜️Owner', url='https://t.me/am_dq_fan')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
